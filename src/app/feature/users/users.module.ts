5cf2e2917e98c07d7e068e1557cf55e3366c2490
Leoneider <leoneider@hotmail.es>
2021-08-26 10:06:26 -0500

    hacker rank test

--- src/app/feature/users/users.module.ts
+++ src/app/feature/users/users.module.ts
@@ -4,7 +4,7 @@ import { SharedModule } from '@shared/shared.module';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { HomeUserComponent } from './home-user/home-user.component';
 import { ListUsersComponent } from './list-users/list-users.component';
-import { NavBarComponent } from '../../shared/components/nav-bar/nav-bar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { UsersRoutingModule } from './users-routing.module';
 
 
